7084e597e7b2b3625546985d6704ae50bcc8d3c4
Dragonetti1 <112328866+Dragonetti1@users.noreply.github.com>
2022-10-19 14:16:41 +0200

    Update dragonetti_focus to arrangement then record.lua

--- Transport/dragonetti_focus to arrangement then record.lua
+++ Transport/dragonetti_focus to arrangement then record.lua
@@ -1,2 +1,7 @@
+-- @description set focus to Arrangement an then record
+-- @author Dragonetti
+-- @version 1.0.0
+-- @about
+--   prevents the deletion of the track instead of the item after recording.
 reaper.SetCursorContext(1,nil)
 reaper.Main_OnCommand(1013,0) 
